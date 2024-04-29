e7e19865caed3ffda4e0d81dc1fd621d1d3f21fd
ooomega55 <gvf55@outlook.com>
2024-04-29 14:43:45 -0400

    tutorial #2 vue

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
   data () {
     return {
-      product: 'Socks'
+      product: 'Socks',
+      image: '/assets/images/socks_blue.jpg',
+      url: 'https://github.com/ooomega55/1_Creating_Vue_App'
     }
   }
 })
